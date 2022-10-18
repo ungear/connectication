63cf3b1e43a4ebb5d4c2f6da5de13329fd11004c
Mikhail Istomin <mikhail_istomin@epam.com>
2022-10-18 23:32:41 +0300

    add Create User endpoint

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,16 @@
 import {
+  BadRequestException,
+  Body,
   Controller,
   Get,
   NotFoundException,
   Param,
   ParseIntPipe,
+  Post,
 } from '@nestjs/common';
 import { UserService } from './user.service';
 import { Profile } from './profile.entity';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Controller('user')
 export class UserController {
@@ -19,5 +23,14 @@ export class UserController {
     const profile = await this.userService.getProfileByUserId(userId);
     if (!profile) throw new NotFoundException('Profile Not Found');
     return profile;
+  }
+
+  @Post()
+  async createUser(@Body() createUserDto: CreateUserDto) {
+    try {
+      return await this.userService.createUser(createUserDto);
+    } catch (error) {
+      throw new BadRequestException('Failed to create a user');
+    }
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -3,15 +3,30 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { Repository } from 'typeorm';
 import { Profile } from './profile.entity';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
 export class UserService {
   constructor(
     @InjectRepository(User) private usersRepository: Repository<User>,
+    @InjectRepository(Profile) private profileRepository: Repository<Profile>,
   ) {}
 
   async findByLogin(username: string): Promise<User | undefined> {
     return this.usersRepository.findOneBy({ username });
+  }
+
+  async createUser(createUserDto: CreateUserDto): Promise<User> {
+    const profile = new Profile();
+    profile.firstName = createUserDto.firstName;
+    profile.lastName = createUserDto.lastName;
+    await this.profileRepository.save(profile);
+    const user = new User();
+    user.username = createUserDto.username;
+    user.password = createUserDto.password;
+    user.profile = profile;
+    await this.usersRepository.save(user);
+    return user;
   }
 
   async getProfileByUserId(userId: number): Promise<Profile | null> {
