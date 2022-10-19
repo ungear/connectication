1ddac71308d2a628ae980d8628754ced7253a16a
Mikhail Istomin <mikhail_istomin@epam.com>
2022-10-19 20:12:19 +0300

    move DB configs in .env file

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,24 +4,29 @@ import { UserModule } from './user/user.module';
 import { PostModule } from './post/post.module';
 import { AuthModule } from './auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     UserModule,
     PostModule,
     AuthModule,
-    // TODO move DB properties in ENV or config
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3307,
-      username: 'root',
-      password: 'root_pwd',
-      // the database must be created manually before the first run
-      database: 'connectication',
-      autoLoadEntities: true,
-      //synchronize: true,
-      //dropSchema: true,
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get<string>('DATABASE_HOST'),
+        port: configService.get<number>('DATABASE_PORT'),
+        username: configService.get<string>('DATABASE_USER'),
+        password: configService.get<string>('DATABASE_PASSWORD'),
+        // the database must be created manually before the first run
+        database: configService.get<string>('DATABASE_NAME'),
+        autoLoadEntities: true,
+        //synchronize: true,
+        //dropSchema: true,
+      }),
+      inject: [ConfigService],
     }),
   ],
   controllers: [AppController],
