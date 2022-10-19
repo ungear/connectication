193b3bb8397cab160ec10d350074a0ad7908dc15
Mikhail Istomin <mikhail_istomin@epam.com>
2022-10-19 20:25:15 +0300

    move JWT configs in .env file

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,17 +4,22 @@ import { UserModule } from '../user/user.module';
 import { PassportModule } from '@nestjs/passport';
 import { LocalStrategy } from './local.strategy';
 import { JwtModule } from '@nestjs/jwt';
-import { jwtConstants } from './constants';
 import { JwtStrategy } from './jwt.strategy';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   providers: [AuthService, LocalStrategy, JwtStrategy],
   imports: [
     UserModule,
     PassportModule,
-    JwtModule.register({
-      secret: jwtConstants.secret,
-      signOptions: { expiresIn: '60s' },
+    ConfigModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: configService.get<string>('JWT_EXPIRES_IN') },
+      }),
     }),
   ],
   exports: [AuthService],
--- src/auth/constants.ts
+++
@@ -1,4 +0,0 @@
-// TODO: move the secret to somewhere
-export const jwtConstants = {
-  secret: 'secretKey',
-};
