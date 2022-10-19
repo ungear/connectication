877497732e0b4c41857bda2474e65837df15a1ea
Mikhail Istomin <mikhail_istomin@epam.com>
2022-10-19 21:59:58 +0300

    add todo

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  // TODO: get port from configs
   await app.listen(3000);
 }
 bootstrap();
