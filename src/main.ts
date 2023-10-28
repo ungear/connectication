4650f5fa41686962a322b6b27d1d13d7498fecbc
Mikhail Istomin <mikhail_istomin@epam.com>
2023-10-28 14:25:48 +0400

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('Connectication API')
+    .setDescription('Connectication API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT);
 }
 bootstrap();
